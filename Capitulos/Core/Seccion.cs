d4804f1de214f6067ca85e3c341750a6da7ee54b
fralvarezz <fralvarez@esei.uvigo.es>
2019-12-11 11:15:36 +0100

    Subidos capitulos

---
+++ Capitulos/Core/Seccion.cs
@@ -0,0 +1,26 @@
+﻿namespace ProyectoCapitulos.Core
+{
+    public class Seccion
+    {
+        public Seccion(string notas, string texto)
+        {
+            this.notas = notas;
+            this.texto = texto;
+        }
+
+        public string notas
+        {
+            get; set;
+        }
+
+        public string texto
+        {
+            get; set;
+        }
+
+        public override string ToString()
+        {
+            return texto + "\n";
+        }
+    }
+}
